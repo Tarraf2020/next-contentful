fcff72c4ecf50457a7dfb5d17e8e437d4a534881
Tarraf2020 <ali.tarraf2017@gmail.com>
2023-05-23 18:16:57 +0300

    test 2

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -6,7 +6,7 @@ const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
-
+ 
 export const getStaticPaths = async () => {
   const res = await client.getEntries({ 
     content_type: "recipe" 
